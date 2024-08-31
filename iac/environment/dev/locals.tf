973fd41ffa949e5136c3ce80c81f654573ffffd6
franco martin <francomartin3@gmail.com>
2024-08-31 10:19:33 -0700

    Adds comments

--- iac/environment/dev/locals.tf
+++ iac/environment/dev/locals.tf
@@ -4,6 +4,7 @@ locals {
   image_tag               = "0.0.1"
   backend_container_image = "${module.ecr_backend.repository.repository_uri}:${local.image_tag}"
   # Las IPs de los devs como CIDR
-  allowed_cidrs        = ["98.164.211.4/32"] # Obtene la tuya con "curl ifconfig.me"
-  crear_recursos_caros = true
+  allowed_cidrs           = ["98.164.211.4/32"] # Obtene la tuya con "curl ifconfig.me"
+  # Deshabilita la creacion del VPC, App Runner y DocumentDB para trabajar en el frontend
+  crear_recursos_caros    = true
 }
